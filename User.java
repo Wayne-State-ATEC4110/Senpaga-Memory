42aaf949690005c59e6afa9516c0bd9dc98d3b88
CalvinBrooks-fn2669 <55375792+CalvinBrooks-fn2669@users.noreply.github.com>
2020-03-24 23:18:30 -0400

    Add files via upload

---
+++ User.java
@@ -0,0 +1,34 @@
+
+public class User {
+	private String userID;
+	private String firstName;
+	private String lastName;
+	
+	
+	public User() {
+	}
+	public User(String userID, String firstName, String lastName) {
+		this.userID = userID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
