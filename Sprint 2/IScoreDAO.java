266c11f79c460788281b5f7eacfd0c40cf06ea0f
CalvinBrooks-fn2669 <55375792+CalvinBrooks-fn2669@users.noreply.github.com>
2020-03-28 22:32:50 -0400

    Revert "Calvin"

--- Sprint 2/IScoreDAO.java
+++
@@ -1,9 +0,0 @@
-import java.sql.SQLException;
-import java.util.List;
-
-public interface IScoreDAO<T> {
-	void createTable() throws SQLException;
-	void save(T t) throws SQLException;
-	List<T> getAll() throws SQLException;
-	List<T> getAll(String param) throws SQLException;
-}
