58d12f50215e1f62113b86d84c2d142bfc703fe6
cthomasn <60117921+cthomasn@users.noreply.github.com>
2020-03-29 01:21:34 -0400

    Adding my unit test

---
+++ MenuSystemTest.java
@@ -0,0 +1,22 @@
+package randomWordGame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MenuSystemTest {
+
+	@SuppressWarnings("static-access")
+	@Test
+	void gameModeTest() {
+		MenuSystem test1 = MenuSystem.getInstance();
+		
+		test1.gameModeMenu();
+		
+		
+		
+	}
+	
+
+}
+
