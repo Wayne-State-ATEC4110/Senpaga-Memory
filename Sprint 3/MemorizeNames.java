7b61a7f4d4980e9b503989f512423273060a7967
CalvinBrooks-fn2669 <55375792+CalvinBrooks-fn2669@users.noreply.github.com>
2020-03-31 12:02:16 -0400

    Add files via upload

--- Sprint 3/MemorizeNames.java
+++ Sprint 3/MemorizeNames.java
@@ -1,175 +1,221 @@
-//author: Logan Jackson
+/**
+ * @author Logan Jackson
+ * Description: MemorizeNames class controls the functionality
+ * for everything needed for the name memorization game.
+*/
+
+package me.logan.senpagamemory;
 
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 
 public class MemorizeNames {
-    private String[] namesInFile;
-    private String[] names;
-    private String[] enterNames;
-    private File file;
-    private String nameType;
-    
-    //I added this variable to make scorekeeping easier
-    double correctSend;
+
+    private static final String DEFAULT_FILE_PATH = "SenpagaDefaultNames.txt";
+    private static final String USER_FILE_PATH = "SenpagaUserNames.txt";
+
+    private ArrayList<String> names = new ArrayList<String>();
+    private ArrayList<String> populatedArray = new ArrayList<String>();
+    private ArrayList<String> namesGuessed = new ArrayList<String>();
 
 
-    public int getAmountLinesInFile(File file) {
-        int numOfLines = 0;
-        try {
-            Scanner reader = new Scanner(file);
-            while (reader.hasNextLine()) {
-                String line = reader.nextLine();
-                numOfLines++;
-            }
-            reader.close();
-        } catch(FileNotFoundException e) {
-            System.out.println("File not found!");
-            e.printStackTrace();
-        }
-        return numOfLines;
-    }
-
-    /*
-        Asks what type of names they want to use
-        Option 1: Memorize random names from pre-given file
-        Option 2: Enter their own names
-    */
-    public String getNameType() {
-        int choice;
+    /**
+     * Description: The startGame method controls the setup
+     * of the game and runs the remaining methods in the order
+     * needed for the game to run properly.
+     */
+    public void startGame() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\nDo you want to use random names or your own names?");
-        System.out.println("1) Random Names\n2) Own Names");
-        System.out.print("Select an option: ");
-        choice = scanner.nextInt();
-        if (choice == 1) {
-            return "Random";
-        } else if (choice == 2) {
-            return "Own";
-        } else {
-            return "Error";
-        }
-    }
-
-    public void populateArrayWithNames() {
-        int counter = 0;
-        String nameType = getNameType();
-        
-        //commenting this out to get the jar to work, could probably use an if statement here to jury rig
-        File file = new File(nameType +  ".txt");
-        //File file = new File("Random.txt");
-        
-        int numOfLines = getAmountLinesInFile(file);
-        namesInFile = new String[numOfLines];
-
-        try {
-            Scanner reader = new Scanner(file);
-            while (reader.hasNextLine()) {
-                String line = reader.nextLine();
-                namesInFile[counter] = line;
-                counter++;
-            }
-            reader.close();
-        } catch(FileNotFoundException e) {
-            System.out.println("File not found!");
-            e.printStackTrace();
-        }
-
-        populateArrayWithRandomNames();
-        //System.out.println("Your file has " + numOfLines + " names.");
-
-    }
-
-    public void populateArrayWithRandomNames() {
         int numOfNames;
-        Random rand = new Random();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nHow many names would you like to memorize?");
-        System.out.print("Enter a number: ");
-        numOfNames = scanner.nextInt();
-        names = new String[numOfNames];
-
-        for (int i = 0; i < numOfNames; i++) {
-            names[i] = namesInFile[rand.nextInt(namesInFile.length)];
+        int nameType = selectNameType();
+        System.out.println("\nHow many names do you want to memorize?\nEnter a number between 1 and 1000");
+        System.out.print("Enter option: ");
+        numOfNames = UserValidation.checkNumber(scanner, 1, 1000);
+        if (nameType == 1) {
+            loadNamesFromFile("Default");
+        } else if (nameType == 2) {
+            loadNamesFromFile("User");
         }
-
+        populateNames(numOfNames);
+        printNames();
+        ClearScreen screen = new ClearScreen();
+        screen.Clear();
+        guessNames();
+        calculateNumberCorrect();
     }
 
-    public void createFile() {
+
+    /**
+     *  Description: The selectNameType method prompts the user to
+     *  select what type of names they want to use.
+     *  Option 1: Default Names (Pre generated file of names already created)
+     *  Option 2: Your Own Names (User defined names written to a file)
+     *
+     *  @return An integer between 1 and 2
+    */
+    public int selectNameType() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nSelect name type");
+        System.out.println("1) Use Default Names");
+        System.out.println("2) Use Your Own Names");
+        System.out.print("Enter option: ");
+        return UserValidation.checkNumber(scanner, 1, 2);
+    }
+
+
+
+
+    /**
+     * Description: The loadNamesFromFile method loads names from either
+     * the Default File Path or the User File Path based on what
+     * the user selected in the selectNameType method.
+     *
+     * @param nameType
+     */
+    public void loadNamesFromFile(String nameType) {
+        populatedArray.clear();
+        File file = new File(DEFAULT_FILE_PATH);
+
+        if (nameType == "Default") {
+            file = new File(DEFAULT_FILE_PATH);
+        } else if (nameType == "User") {
+            writeToFile();
+            file = new File(USER_FILE_PATH);
+        }
+
         try {
-            File file = new File("customnames.txt");
-            if (file.createNewFile()) {
-                System.out.println("File created");
-            } else {
-                System.out.println("File already exits");
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                populatedArray.add(line);
             }
-        } catch (IOException e) {
-            System.out.println("Error occured");
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found!");
             e.printStackTrace();
         }
     }
 
 
+
+    /**
+     * Description: Populates array list with random names
+     * from another array list containing the entire file
+     * of names.
+     *
+     * @param numberOfNames
+     */
+    public void populateNames(int numberOfNames) {
+        Random rand = new Random();
+
+        for(int i = 0; i < numberOfNames; i++) {
+            names.add(populatedArray.get(rand.nextInt(populatedArray.size())));
+        }
+    }
+
+
+    /**
+     * Description: Writes user defined names to a file
+     * until the user types "quit".
+    */
+    public void writeToFile() {
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        FileWriter writeObj;
+        PrintWriter printObj;
+
+        System.out.println("Type 'QUIT' when you are done entering names.");
+        try {
+            while(!name.equalsIgnoreCase("QUIT")) {
+                writeObj = new FileWriter(USER_FILE_PATH, true);
+                printObj = new PrintWriter(writeObj);
+
+
+                System.out.print("Enter name: ");
+                name = scanner.nextLine();
+
+                if (!name.equalsIgnoreCase("quit")) {
+                    printObj.println(name);
+                }
+                printObj.close();
+            }
+        } catch(Exception e) {
+            System.out.println("Error");
+        }
+
+    }
+
+
+    /**
+     * Description: printNames function prints names
+     * with a selected amount of names per row defined
+     * by the user. Then creates a delay in seconds before
+     * the screen will be cleared also defined by the user.
+    */
     public void printNames() {
         Scanner scanner = new Scanner(System.in);
-        int seconds;
+        int counter = 0;
+
         System.out.println("\nHow long do you want to view names (in seconds): ");
-        System.out.print("Enter number: ");
-        seconds = scanner.nextInt();
+        System.out.print("Enter number between 1 and 1000: ");
+        int seconds = UserValidation.checkNumber(scanner, 1, 1000);
+
+        System.out.println("How many names do you want per row: ");
+        System.out.print("Enter number between 1 and 10: ");
+        int numOfNamesPerRow = UserValidation.checkNumber(scanner, 1, 10);
 
         System.out.println("\nYou have " + seconds + " seconds to remember the following names in order: \n");
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
+        for (int i = 0; i < names.size(); i++) {
+            if (counter < numOfNamesPerRow) {
+                System.out.print(names.get(i) + " ");
+                counter++;
+            }
+            if (counter == numOfNamesPerRow) {
+                System.out.println();
+                counter = 0;
+            }
         }
         try {
             Thread.sleep(seconds * 1000);
         } catch(InterruptedException e) {
             e.printStackTrace();
         }
-        clearScreen();
     }
 
 
+    /**
+     * Description: Prompts users to enter names and
+     * stores them in an Array List.
+    */
     public void guessNames() {
         Scanner scanner = new Scanner(System.in);
-        enterNames = new String[names.length];
         System.out.println("\nGuess the names previously printed");
 
-        for (int i = 0; i < enterNames.length; i++) {
+        namesGuessed.clear();
+        for (int i = 0; i < names.size(); i++) {
             System.out.print((i + 1) + ": ");
-            enterNames[i] = scanner.nextLine();
+            namesGuessed.add(i, scanner.nextLine());
         }
-
-
     }
 
-
-    public double calculateNumberCorrect() {
-        double correct = 0;
-        for(int i=0; i < enterNames.length; i++) {
-            if (enterNames[i].equals(names[i])) {
+    /**
+     * Description: Calculates the number of names guessed
+     * correct and prints it to the console.
+    */
+    public void calculateNumberCorrect() {
+        int correct = 0;
+        for (int i = 0; i < namesGuessed.size(); i++) {
+            if (namesGuessed.get(i).equals(names.get(i))) {
                 correct++;
             }
         }
-        
-        this.correctSend = correct;
-        return ((correct / names.length) * 100);
-    }
 
-    public void printNumberCorrect() {
-        System.out.println("\nYou got a " + calculateNumberCorrect() + "%\n");
-    }
+        System.out.println("You got " + correct + " correct out of " + names.size() + "\n");
 
-    public void printGuess() {
-        for (int i = 0; i < enterNames.length; i++) {
-            System.out.println(enterNames[i]);
-        }
-    }
-
-    public void clearScreen() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("");
-        }
+        // Can return a value if needed for leaderboards in future.
+        //return ((correct / names.size()) * 100);
     }
 
 
--- Sprint 3/Options.java
+++ Sprint 3/Options.java
@@ -1,49 +1,67 @@
+/**
+ * @author Logan Jackson
+ * Description: Options class is used to create
+ * an options menu for users to navigate through the program.
+*/
+
+package me.logan.senpagamemory;
+
 import java.util.Scanner;
 
 public class Options {
-    private String[] optionList = new String[8]; //may have to change this
+    private String[] optionList = new String[5]; //may have to change this
     private int option;
 
+
+    /**
+     * Description: Options constructor initializes the Options
+     * object and defines the option items.
+    */
     public Options() {
         optionList[0] = "1) Memorize Names";
         optionList[1] = "2) Memorize Cards";
         optionList[2] = "3) Memorize Words";
         optionList[3] = "4) Memorize Numbers";
-        optionList[4] = "5) Choose a User";
-        optionList[5] = "6) Add a User";
-        optionList[6] = "7) Delete a User";
-        optionList[7] = "8) Quit";
+        optionList[4] = "5) Quit";
     }
 
+    /**
+     * Description: method to get the option
+     * selected.
+     *
+     * @return this.option
+    */
     public int getOption() {
         return this.option;
     }
 
+    /**
+     * Description: Prints a welcome message and
+     * a list of options for the user to select
+     * from.
+    */
     public void print() {
-    	ManagingUser current = new ManagingUser();
-    	
-        System.out.println("Welcome to Senpaga Memory\n\nSelect an option to continue");
-        System.out.print("The current user is: ");
-        current.getCurrentUser();
-        System.out.println();
-        
-        
+        System.out.println("Welcome to Senpaga Memory\n\nSelect an option to continue\n");
         for (int i = 0; i < optionList.length; i++) {
             System.out.println(optionList[i]);
         }
         System.out.println("");
     }
 
+    /**
+     * Description: Prompts the user to select an
+     * option from the options menu.
+     *
+     * @return optionSelect
+    */
     public int selectOption() {
         int optionSelect;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Select an option: ");
-        optionSelect = scanner.nextInt();
+        optionSelect = UserValidation.checkNumber(scanner, 1, 5);
         this.option = optionSelect;
         return optionSelect;
     }
-    
-   
 
 
 }
